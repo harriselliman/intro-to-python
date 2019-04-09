3c576f3962a8f356b2e7db2b46fd7978eaf64cd3
harriselliman <harris.elliman@hotmail.co.uk>
2019-04-09 12:12:53 +0100

    add basic calculator

---
+++ helloworld.py
@@ -0,0 +1,16 @@
+# basic calculator
+
+num1 = float(input("Enter first number: "))
+op = input("Enter operator: ")
+num2 = float(input("Enter another number: "))
+
+if op == "+":
+    print(num1 + num2)
+elif op == "-":
+    print(num1 - num2)
+elif op == "/":
+    print(num1 / num2)
+elif op == "*":
+    print(num1 * num2)
+else:
+    print("Invalid operator")
