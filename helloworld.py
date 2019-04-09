0b1ce8fe5dc2ccea491be2ca1c7e1b2a7ab4acf8
harriselliman <harris.elliman@hotmail.co.uk>
2019-04-09 16:04:35 +0100

    add run_test function including for loop and if statement to keep track of correct answers

--- helloworld.py
+++ helloworld.py
@@ -125,3 +125,11 @@ questions = [
     Question(question_prompts[1], "a"),
     Question(question_prompts[2], "c")
 ]
+
+def run_test(questions):
+    score = 0
+    for question in questions:
+        answer = input(question.prompt)
+        if answer == question.answer:
+            score += 1
+    print("You got " + str(score) + "/" + str(len(questions)) + " correct.")
