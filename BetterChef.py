4870a3b65b3d0f4dd55bacd87c3e17fdb31509ee
harriselliman <harris.elliman@hotmail.co.uk>
2019-04-09 16:37:36 +0100

    add BetterChef class which inherits functions from Chef

---
+++ BetterChef.py
@@ -0,0 +1,9 @@
+from Chef import Chef
+
+class BetterChef(Chef):
+
+    def make_special_dish(self):
+        print("The chef makes soy sauce pork.")
+
+    def make_burger(self):
+        print("The chef makes a burger.")
