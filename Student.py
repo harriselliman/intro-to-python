99e44dc5bef5401553389e4b0ad0e0daf6f85dea
harriselliman <harris.elliman@hotmail.co.uk>
2019-04-09 16:13:47 +0100

    add on_honor_roll function to Student class

--- Student.py
+++ Student.py
@@ -4,3 +4,10 @@ class Student:
         self.name = name
         self.major = major
         self.gpa = gpa
+
+    def on_honor_roll(self):
+        if self.gpa >= 3.5:
+            return True
+        else:
+            return False
+
