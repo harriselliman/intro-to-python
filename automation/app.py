5445cae94902abb56f341bfb8c0a8aaaab6b4177
harriselliman <harris.elliman@hotmail.co.uk>
2019-04-11 19:05:58 +0100

    create barchart and pass in values as data

--- automation/app.py
+++ automation/app.py
@@ -10,4 +10,13 @@ for row in range(2, sheet.max_row +1):
     corrected_price_cell = sheet.cell(row, 4)
     corrected_price_cell.value = corrected_price
 
+values = Reference(sheet,
+min_row=2,
+max_row=sheet.max_row,
+min_col=4
+max_col=4)
+
+chart = BarChart()
+chart.add_data(values)
+
 wb.save("transactions2.xlsx")
