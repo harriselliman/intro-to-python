679af4730aefc1bd8ea416af658e7dd0f5da5640
harriselliman <harris.elliman@hotmail.co.uk>
2019-04-11 18:59:32 +0100

    add corrected prices to corrected price cells

--- automation/app.py
+++ automation/app.py
@@ -7,3 +7,4 @@ for row in range(2, sheet.max_row +1):
     cell = sheet.cell(row, 3)
     corrected_price = cell.value * 0.9
     corrected_price_cell = sheet.cell(row, 4)
+    corrected_price_cell.value = corrected_price
